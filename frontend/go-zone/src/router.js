0b7b7b62136c2f360f38ff960ea88de2b568363d
chisfee <shaohui.chen@ecreditpal.com>
2019-11-28 23:20:40 +0800

    test route

--- frontend/go-zone/src/router.js
+++ frontend/go-zone/src/router.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import LoginPage from './routes/LoginPage/loginPage';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Switch>
+      <div>
         <Route path="/" exact component={IndexPage} />
-      </Switch>
+        <Route path="/login" exact component={LoginPage} />
+      </div>
     </Router>
   );
 }
