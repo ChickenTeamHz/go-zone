200118de6300773940c99310f687f9c03292b639
Chisfee <>
2019-11-29 18:08:24 +0800

    update

--- frontend/go-zone/src/components/HeadBar/index.js
+++ frontend/go-zone/src/components/HeadBar/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { connect } from 'dva'
-import styles from './headBar.css'
+import styles from './index.css'
 
 function HeadBar() {
     return (
         <div className={ styles.head_bar }>
-
+            <div className={ styles.page_name }>Go-Zone</div>
         </div>
     );
 }
--- frontend/go-zone/src/router.js
+++ frontend/go-zone/src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Router, Route } from 'dva/router';
 import IndexPage from './routes/IndexPage';
-import LoginPage from './routes/LoginPage/loginPage';
+import LoginPage from './routes/LoginPage';
+import HeaderBar from './components/HeadBar'
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <div className="wrapper">
+        <HeaderBar></HeaderBar>
         <Route path="/" exact component={IndexPage} />
         <Route path="/login" exact component={LoginPage} />
       </div>
