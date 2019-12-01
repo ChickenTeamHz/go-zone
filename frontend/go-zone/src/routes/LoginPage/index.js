26fb7d21964bd54d9be0d2721ecff91b0c1e2b0f
NormanChen-csh <249507273@qq.com>
2019-12-01 19:26:12 +0800

    'route'

--- frontend/go-zone/src/routes/LoginPage/index.js
+++ frontend/go-zone/src/routes/LoginPage/index.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { connect } from 'dva'
 import styles from './index.css'
 
-function LoginPage() {
-    return (
-        <div className={ styles.login_page }>
-            <p className={ styles.login_title }>Login</p>
-            <div className={styles.login_form}>
-
+class LoginPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loginType: Number(this.props.location.query.type)
+        }
+    }
+    // getDerivedStateFromProps() {
+    //     console.log(this)
+    // }
+    // componentDidMount() {
+    //     console.log(this)
+    // }
+    render() {
+        return (
+            <div className={ styles.login_page }>
+                <p className={ styles.login_title }>
+                    {
+                        this.state.loginType === 1 ?  'Login': 'Register'
+                    }
+                </p>
+                <div className={styles.login_form}>
+    
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 LoginPage.propTypes = {
