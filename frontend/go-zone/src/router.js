d9868b70361b521c5e5e97be9bd7afb8f8b19b96
Chisfee <>
2019-12-04 10:47:50 +0800

    路由封装成功

--- frontend/go-zone/src/router.js
+++ frontend/go-zone/src/router.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { Router, Route } from 'dva/router';
-import IndexPage from './routes/IndexPage';
-import LoginPage from './routes/LoginPage';
+import { Route, Switch, routerRedux } from 'dva/router';
+import RouteArr from './routes/routeConfig'
+import dynamic from 'dva/dynamic'
+
+
 import HeaderBar from './components/HeadBar'
 
-function RouterConfig({ history }) {
+const { ConnectedRouter } = routerRedux
+
+function RouterConfig({ history, app }) {
+  let routes = RouteArr
+
   return (
-    <Router history={history}>
-      <div className="wrapper">
-        <HeaderBar></HeaderBar>
-        <Route path="/" exact component={IndexPage} />
-        <Route path="/login" exact component={LoginPage} />
-      </div>
-    </Router>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <div>
+          {
+            routes.map(({ path, name, layout, ...dynamics }) => {
+              const Component = dynamic({ app, ...dynamics })
+              return (
+                <Route path={path} key={name} exact
+                  render={(props) => {
+                    if(layout) {
+                      return (
+                        <div>
+                          <HeaderBar/>
+                          <Component {...props} />
+                        </div>
+                      )
+                    }
+                    return (<Component {...props} />)
+                  }} />
+              )
+            })
+          }
+        </div>
+      </Switch>
+    </ConnectedRouter>
   );
 }
 
